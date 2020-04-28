99e1edc71a010192fc33609358133a5d860dd34c
Vix Hi <vixhi@VixdeMacBook-Pro.local>
2020-04-28 13:42:49 +0800

    初始化线性表

---
+++ data/line_list_unit.go
@@ -0,0 +1,41 @@
+package main
+
+const MAXSIZE int = 20
+type ElemType int
+
+type Sqlist struct {
+	data [MAXSIZE]ElemType
+	length int
+}
+
+func initList()  {
+
+}
+
+func listEmpty()  {
+
+}
+
+func clearList()  {
+
+}
+
+func getElem()  {
+
+}
+
+func locateElem()  {
+
+}
+
+func listInsert()  {
+
+}
+
+func listDelete()  {
+
+}
+
+func listLength()  {
+
+}
